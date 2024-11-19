221120836537e67630712b808988fdfcf399be65
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-11-19 17:36:27 +0100

    simple form

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,33 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const components = [
+    {
+      id: 1,
+      artTitle: "5 Consigli per una vita più produttiva",
+      artText: "Lorem ipsum, siat amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
+    },
+    {
+      id: 2,
+      artTitle: "Come scegliere il tuo smartphone ideale",
+      artText: "Lorem ipsum, siat amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
+    },
+    {
+      id: 3,
+      artTitle: "La storia di un imprenditore che ha cambiato la sua vita",
+      artText: "Lorem ipsum, siat amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
+    }
+  ];
+
+  console.log(components);
+
+  const [title, setTitle] = useState('Title')
+  const [article, setArticle] = useState('lorem ipsum asfda dagsg eagadzgaz.')
+
+
+  function handleTitleChange(e) {
+    console.log(e);
+    setTitle(e.target.value)
+  }
+
+  function handleArticleChange(e) {
+    console.log(e);
+    setArticle(e.target.value)
+  }
 
   return (
     <>
+
+      <h1>Inserisci il titolo</h1>
+      <input type="text" name='title' id='title' value={title} onChange={e => setTitle(e.target.value)} />
+
+      <h1>Inserisci l'articolo</h1>
+      <input type='text' name='article' id='article' value={article} onChange={e => setArticle(e.target.value)} />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="submit" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="components">
+        {components.map(component =>
+          <div key={component.id}>
+            <div>
+              <h2>{component.artTitle}</h2>
+              <div>{component.artText}</div>
+            </div>
+          </div>)
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
